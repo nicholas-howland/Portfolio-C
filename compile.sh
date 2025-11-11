317080867cddbe3918607ad14f763532b9e05004
Nicholas Howland <94145193+nicholas-howland@users.noreply.github.com>
2025-11-11 11:49:10 -0800

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Prechecks to make sure the files exist
+if [ ! -d "bin" ]; then mkdir bin; fi
+if [ ! -d "source" ]; then mkdir source; fi
+if [ ! -f "source/00-template.c" ];
+then
+echo "#include <stdio.h>
+
+int main() {
+    printf(\"Hello, World!\\n\");
+    return 0;
+}
+" > source/00-template.c
+fi
+
+## handle name i/o
+PRONAME=$1
+if [ ! -f "source/$PRONAME.c" ]; then cp source/00-template.c source/$PRONAME.c;fi
+#nano source/$PRONAME.c
+gcc source/$PRONAME.c -o bin/$PRONAME
+valgrind bin/$PRONAME
