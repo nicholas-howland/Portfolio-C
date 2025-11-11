210e945280f541b9dce99feec3c8804132d8c1f3
Nicholas Howland <94145193+nicholas-howland@users.noreply.github.com>
2025-11-11 13:27:28 -0800

    Update compile.sh
    added command line options

--- compile.sh
+++ compile.sh
@@ -18,6 +18,16 @@ fi
 ## handle name i/o
 PRONAME=$1
 if [ ! -f "source/$PRONAME.c" ]; then cp source/00-template.c source/$PRONAME.c;fi
-#nano source/$PRONAME.c
+if [ $1 == "-e" ]; then PRONAME=$2; nano source/$PRONAME.c;
+gcc source/$PRONAME.c -o bin/$PRONAME;
+valgrind bin/$PRONAME;
+elif [ $1 == "-c" ]; then PRONAME=$1; nano source/$PRONAME.c;
 gcc source/$PRONAME.c -o bin/$PRONAME
 valgrind bin/$PRONAME
+else
+echo "compile.sh [-c|-e|-h] source-name"
+echo "compilation script"
+echo "    -e    edit an existing file not including the .c extension and compile it"
+echo "    -c    compile only"
+echo "    -h    show this message"
+fi
