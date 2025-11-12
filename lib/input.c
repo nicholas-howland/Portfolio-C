84b680373a09ea41f377a180cc56dc81ec82a26a
Nicholas Howland <94145193+nicholas-howland@users.noreply.github.com>
2025-11-11 18:07:57 -0800

    Update input.c
    updated get safe int operation flow and functionized it

--- lib/input.c
+++ lib/input.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int getIntSafely(){
+// get int safely function
+    char test[10];// it is one biger than we need it because the \0 char will always be the last one
+    int trueSize=0;
+    fgets(test,sizeof(test),stdin);
+    int size=sizeof(test)-1;// adjust for integer addressing
+
+    // detect a buffer overflow attempt only read the first chars that were entered into
+    // the input
+    while(trueSize<size && test[trueSize]!=10){
+    // check to see if there is a newline character at the current place in the array
+        trueSize++;
+        if(trueSize>=size){
+        // if the new truesize value is larger than the actual size of the array
+        // enter a for loop until the newline character is read
+            int c; while ((c = getchar()) != '\n' && c != EOF) { }
+        }
+    }
+
+// print out some stats about the new array including its integer value if possible
+    printf("size of array: %d\n",sizeof(test));
+    printf("last char: %c\n",test[size-1]);
+    printf("last decimal: %d\n",test[size-1]);
+    // atoi will convert chars to ints up to the first non decimal character
+    printf("string to int: %d\n",atoi(test));
+    printf("True size of array: %d\n",trueSize);
+    return atoi(test);
+}
+
 // main execution area
 int main(void) {
-
-// Example of semi-safe input  
-  char test[10];
-  printf("%s\n",fgets(test,sizeof(test),stdin));
-  // clear buffer that is being streamed to functions through stdin
-  int c;
-  while ((c = getchar()) != '\n' && c != EOF) { }
-  // continue getting input
-  printf("%s\n",fgets(test,sizeof(test),stdin));
-
-
+   int x = getIntSafely();
+// continue getting input to test for buffer overflows
+   printf("returned value: %d\nIf you can enter stuff here then the buffer overflow was mitigated\n",x);
+   char test[2];
+   fgets(test,sizeof(test),stdin);
 }
